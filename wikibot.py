c6374007d13f3b9be3d2ba724a7a3b9f2edaa57a
Manukumar9319 <manukumar9319@gmail.com>
2025-11-16 16:33:13 +0530

    removing unwanted code

--- wikibot.py
+++ wikibot.py
@@ -1,6 +1,4 @@
 import wikipedia 
-result = wikipedia.summary("facebook", sentences=10)
-print(result)
 
 def scrape(name="Microsoft", length=1):
     result = wikipedia.summary(name, sentences=length)
