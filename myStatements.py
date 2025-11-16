a20163179513dc8f6ed32f5ab056ff4ea72125cc
Manukumar9319 <manukumar9319@gmail.com>
2025-11-16 15:35:27 +0530

    adding fies to repo

---
+++ myStatements.py
@@ -0,0 +1,10 @@
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {"drink": "coffee", "milk": "whole"}
+
+
+for item in my_list:
+    print(f"My favourite thing is to eat: {item}")
+
+
+for _, value in my_dict.items():
+    print(f"My favourite thing is to drink {value}")
